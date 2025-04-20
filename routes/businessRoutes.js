c71e9311bea66ce9f0cbeae8d4269eb4115eea38
AsithaZD <asitha@zdatai.com>
2025-04-20 22:07:59 +0530

    business api development

--- routes/businessRoutes.js
+++ routes/businessRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const { addEvent, searchEvents, updateEvent } = require('../controllers/businessController');
+const { addEvent, searchEvents, getEventById, disableEvent, getEventsByBusinessId } = require('../controllers/businessController');
 const router = express.Router();
 
 router.post('/addEvent', addEvent);
 router.get('/searchEvents', searchEvents);
-router.put('/updateEvent/:eventId', updateEvent);
+router.get('/getEventById/:id', getEventById);
+router.put('/disableEvent/:id', disableEvent);
+router.get('/eventsList/:id', getEventsByBusinessId);
 
 module.exports = router;
